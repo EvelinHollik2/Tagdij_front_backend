5a90124f7c090538922933ce536030f811abdfac
EvelinHollik2 <hollikevi@gmail.com>
2024-03-20 09:56:22 +0100

    main

--- TagdijFrontend/tagdij.js
+++ TagdijFrontend/tagdij.js
@@ -5,18 +5,38 @@ document.addEventListener('DOMContentLoaded', function () {
     const updateButton = document.getElementById('update');
     const deleteButton = document.getElementById('delete');
 
-    insertButton.addEventListener('click', function () {
+    insertButton.addEventListener('click', async function () {
         const formm = new FormData(document.getElementById('formm'));
         let baseUrl = "http://localhost/Tagdij_front_backend/TagdijBackend/index.php?ugyfel";
         let options = {
             method: 'POST',
             mode: "cors",
-            headers: {
-            },
             body: formm
         };
-        let response = fetch(baseUrl, options);
+        let response = await fetch(baseUrl, options);
         alert("Sikeres feltöltés");
-    })
+    });
 
+    readButton.addEventListener('click', async function () {
+        let baseUrl = "http://localhost/Tagdij_front_backend/TagdijBackend/index.php?ugyfel";
+        let options = {
+            method: 'GET',
+            mode: "cors",   
+        };
+        let response = await fetch(baseUrl, options);
+        if (response.ok) {
+            let data = await response.json();
+            console.log(data);
+        } else {
+            console.error('Hiba a szerver válaszában');
+        };
+    });
+
+    updateButton.addEventListener('click', async function () {
+        let baseUrl = "http://localhost/Tagdij_front_backend/TagdijBackend/index.php?ugyfel";
+        let options = {
+            method: 'PUT',
+            mode: "cors",   
+        };
+    });
 });
