f1d2d85147e115c9377ce70c2e8c1a549ee0d4bc
EvelinHollik2 <hollikevi@gmail.com>
2024-03-20 10:27:16 +0100

    main

--- TagdijFrontend/tagdij.js
+++ TagdijFrontend/tagdij.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const readButton = document.getElementById('read');
     const updateButton = document.getElementById('update');
     const deleteButton = document.getElementById('delete');
-
+    const dolgozoForm = document.getElementById('dolgozoForm');
+    const dolgozolista = document.getElementById('dolgozolista');
     insertButton.addEventListener('click', async function () {
         const formm = new FormData(document.getElementById('formm'));
         let baseUrl = "http://localhost/Tagdij_front_backend/TagdijBackend/index.php?ugyfel";
@@ -18,25 +19,57 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     readButton.addEventListener('click', async function () {
+        dolgozoForm.classList.add('d-none');
+        dolgozoForm.classList.remove('d-none');
         let baseUrl = "http://localhost/Tagdij_front_backend/TagdijBackend/index.php?ugyfel";
         let options = {
             method: 'GET',
-            mode: "cors",   
+            mode: "cors",
         };
         let response = await fetch(baseUrl, options);
         if (response.ok) {
             let data = await response.json();
-            console.log(data);
+            dolgozokListazasa(data);
         } else {
             console.error('Hiba a szerver válaszában');
         };
     });
 
-    updateButton.addEventListener('click', async function () {
-        let baseUrl = "http://localhost/Tagdij_front_backend/TagdijBackend/index.php?ugyfel";
-        let options = {
-            method: 'PUT',
-            mode: "cors",   
-        };
-    });
+    function dolgozokListazasa(dolgozok) {
+        let dolgozokDiv = document.getElementById('ugyfellista');
+        let tablazat = dolgozoFejles();
+        for (let dolgozo of dolgozok) {
+            tablazat += dolgozoSor(dolgozo);
+
+        }
+        dolgozokDiv.innerHTML = tablazat + '</tbody></table>';
+    }
+    function dolgozoSor(dolgozo) {
+        let sor = `<tr>
+        <td>${dolgozo.azon}</td>
+        <td>${dolgozo.nev}</td>
+        <td>${dolgozo.szulev}</td>
+        <td>${dolgozo.irszam}</td>
+        <td>${dolgozo.orsz}</td>
+        <td><button type="button" class="btn btn-outline-secondary" onclick="adatBetoltes(${dolgozo.azon})"><i class="fa-regular fa-hand-point-left"></i></button></td>
+    </tr>`;
+    return sor;
+    }
+
+
+    function dolgozoFejles() {
+        let fejlec = `<table class="table table-sprited">
+        <thead>
+            <tr>
+                <th>Azonosító</th>
+                <th>Név</th>
+                <th>Születési év</th>
+                <th>Irányítószám</th>
+                <th>Ország</th>
+                <th>Művelet</th>
+            </tr>
+        </thead>
+        <tbody>`;
+    }
+
 });
